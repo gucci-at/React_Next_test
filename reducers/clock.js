e15e2de6120d8ab2a1623ef6dffff5898ed3a9a8
gucci <bello.99888@gmail.com>
2020-05-04 14:54:51 +0900

    reducer モジュール登録

---
+++ reducers/clock.js
@@ -0,0 +1,8 @@
+const clock = (state = { lastUpdate: 0, light: false }, action) => {
+    switch (action.type) {
+      case 'TICK': return { lastUpdate: action.ts, light: !!action.light }
+      default: return state
+    }
+  }
+
+  export default clock;
