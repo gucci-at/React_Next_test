707901b8aabc7fd80669e382a2af5293a6a5bbf0
gucci <bello.99888@gmail.com>
2020-05-06 09:40:13 +0900

    ストアとレデューサーのモジュール分離

--- reducers/counter.js
+++ reducers/counter.js
@@ -1,9 +1,23 @@
-export default function counter(state = 0, action) {
+const initial = {
+      message:'START',
+      count: 0
+    }
+
+export default function counter(state = initial, action) {
+    console.table(state);
     switch (action.type) {
     case 'INCREMENT':
-            return state + 1;
+        return Object.assign({}, state, {
+            count: state.count + 1
+            });
     case 'DECREMENT':
-            return state - 1;
+        return Object.assign({}, state, {
+            count: state.count - 1
+            });
+    case 'RESET':
+        return Object.assign({}, state, {
+            count: 0
+            });
     default:
         return state;
     }
