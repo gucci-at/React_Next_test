c19f14b97314643d979f08e6edfbeca4d15e0e1f
gucci <bello.99888@gmail.com>
2020-05-06 09:56:23 +0900

    Districtsコンポーネントの追加

--- components/sagas.js
+++ components/sagas.js
@@ -3,22 +3,28 @@ import { receivePosts, REQUEST_POSTS } from './actions';
 import { call, fork, takeLatest, put } from 'redux-saga/effects';
 
 function fetchCityCodes(prefCode) {
+  console.log('request sending.');
   const URL = `https://www.land.mlit.go.jp/webland/api/CitySearch?area=${prefCode}`;
   return fetch(URL)
     .then(response => response.json())
     .then(json => {
       const data = json.data;
+      console.table(data);
       return { data };
     });
 }
 
 function* fetchPostsIfNeeded(action) {
   // 引数で与えられた関数を実行し、Promiseの完了を待つ
+  console.log('fetchPostsIfNeeded:' + action.prefCode);
   const { data } = yield call(fetchCityCodes, action.prefCode);
   yield put(receivePosts(data));
 }
-
-export default function* rootSaga() {
+function* watchPostsAsync() {
   // 実行中の処理があったら中断し、新しく処理を開始する
-  yield takeLatest(REQUEST_POSTS, fetchPostsIfNeeded);
+  yield takeLatest('REQUEST_POSTS', fetchPostsIfNeeded);
+}
+export default function* rootSaga() {
+  yield fork(watchPostsAsync);
+  //非同期処理はここに追加
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
+import React, {Component} from 'react';
 import Link from 'next/link';
 import Counter from '../components/Counter';
+import Counter_5_12 from '../components/Counter_5_12';
+import Clock, {startClock} from '../components/Clock';
+import Districts from '../components/Districts';
 import style from '../static/Style';
 
-export default () =><div>
-  {style}
-  <h1>Next.js</h1>
-  <p>Welcome to next.js!</p>
-  <hr />
-  <div>
-    <Counter />
-    <Link href="/other">
-      <button>Go to Other page &gt;&gt;</button>
-    </Link>
+export default ()=> <div>
+    {style}
+    <h1>Next.js</h1>
+    <p>Welcome to next.js!</p>
+    <hr />
+    <div>
+      <Counter />
+      <Counter_5_12 />
+      <Districts />
+      <Link as="/districts" href="/other">
+        <button>Go to Other page &gt;&gt;</button>
+      </Link>
+    </div>
   </div>
-</div>
+
